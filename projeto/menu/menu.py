c780abbf2e5b2850a387f46e461409ec704c00d5
Kayky-Eduardo <kayky002a@gmail.com>
2025-05-08 20:57:41 -0300

    Tentativa e erro

--- projeto/menu/menu.py
+++ projeto/menu/menu.py
@@ -5,23 +5,18 @@ from kivy.graphics import Rectangle
 from kivy.uix.widget import Widget
 from kivy.graphics import Color
 from kivy.graphics import Line
+from kivy.uix.screenmanager import Screen
 
-class Encostar(FloatLayout, Widget):
+class TelaSecundaria(Screen):
+    pass
+
+class Menu(FloatLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def on_touch_down(self, touch):
-        print("Mouse Down", touch)
-
-    def on_touch_move(self, touch):
-        print("Mouse Move", touch)
-
-    def on_touch_up(self, touch):
-        print("Mouse UP", touch)
-
 class MenuApp(App):
     def build(self):
-        return Encostar()
+        return Menu()
 
 if __name__ == '__main__':
     MenuApp().run()
