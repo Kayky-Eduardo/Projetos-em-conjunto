47a66084ad5cd12a8587361fd2c44058bd3e227d
Kayky-Eduardo <kayky002a@gmail.com>
2025-05-19 08:25:43 -0300

    mudança cadastro

--- projeto/cadastros/encomenda/cadastro_encomenda.py
+++ projeto/cadastros/encomenda/cadastro_encomenda.py
@@ -17,24 +17,25 @@ class Cadastro_Encomenda(GridLayout):
         self.tipo_contrato = None
        
     def adicionar_banco(self):
-        cargo_id_validacao = self.cargo_id.text
-        nome_cargo_validacao = self.nome_cargo.text
-        tipo_contrato_validacao = self.tipo_contrato
-        setor_id_validacao = self.setor_id.text
+        cliente_cpf_validacao = self.cliente_cpf.text
+        produto_id_validacao = self.produto_id.text
+        qntd_produto_validacao = self.qntd_produto.text
+        data_pedido_validacao = self.data_pedido.text
+        data_entrega_validacao = self.data_entrega.text
         
-        # try:
-        #     # conexao = sqlite3.connect('BD/projeto.db')
-        #     # cursor = conexao.cursor()
-        #     # cursor.execute('''
-        #     #     INSERT INTO cargo (cargo_id, nome_cargo, tipo_contrato, setor_id)
-        #     #     VALUES (?, ?, ?, ?)
-        #     # ''', (cargo_id_validacao, nome_cargo_validacao, tipo_contrato_validacao, setor_id_validacao))
-        #     # conexao.commit()
-        #     # conexao.close()
-        #     # self.enviar.text = 'Cadastro realizado com sucesso!'
+        try:
+            conexao = sqlite3.connect('BD/projeto.db')
+            cursor = conexao.cursor()
+            cursor.execute('''
+                INSERT INTO cargo (cliente_cpf, produto_id, qntd_produto, data_pedido, data_entrega)
+                VALUES (?, ?, ?, ?, ?)
+            ''', (cliente_cpf_validacao, produto_id_validacao, qntd_produto_validacao, data_pedido_validacao, data_entrega_validacao))
+            conexao.commit()
+            conexao.close()
+            self.enviar.text = 'Cadastro realizado com sucesso!'
             
-        # except sqlite3.Error as e:
-        #     self.enviar.text = f'Erro: {e}'
+        except sqlite3.Error as e:
+            self.enviar.text = f'Erro: {e}'
                     
 
 class Cadastro_EncomendaApp(App):
