9ab6370fc606571decb30f9849300718f67019f2
Davi-Melo-Ferreira <davidemeloferreira13@gmail.com>
2025-05-19 20:47:48 -0300

    estoque

--- projeto/estoque/estoque.py
+++ projeto/estoque/estoque.py
@@ -5,6 +5,8 @@ from kivy.lang import Builder
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 
 class Estoque(Screen):
     def __init__(self, **kwargs):
@@ -16,10 +18,17 @@ class Estoque(Screen):
         # Busca produtos do banco
         conexao = sqlite3.connect('BD/projeto.db')
         cursor = conexao.cursor()
-        cursor.execute('SELECT produto.produto_id, produto.nome_produto, produto.tipo_produto, estoque.qntd_produto FROM produto JOIN estoque ON produto.produto_id = estoque.produto_id')
+        cursor.execute('SELECT produto.produto_id, produto.nome_produto, estoque.qntd_produto FROM produto JOIN estoque ON produto.produto_id = estoque.produto_id')
         cursor.row_factory = sqlite3.Row # para acessar colunas pelo nome (ex: produto['nome_produto'])
         produtos = cursor.fetchall()
         conexao.close()
+
+        for produto in produtos:
+            linha = BoxLayout(orientation='horizontal', size_hint_y=None, height=30)
+            nome_label = Label(text=produto['nome_produto'])
+            quantidade_label = Label(text=str(produto['qntd_produto']))
+            linha.add_widget(nome_label)
+            linha.add_widget(quantidade_label)
 
 # para rodar o código como se fosse o principal
 class EstoqueApp(App):
