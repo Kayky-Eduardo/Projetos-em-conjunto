606a1fe62b97a615b269d1825984dc804f274b8f
Kayky-Eduardo <kayky002a@gmail.com>
2025-05-19 21:24:05 -0300

    cadastro produto

--- BD/verificacao_sqlite.py
+++ BD/verificacao_sqlite.py
@@ -2,6 +2,6 @@ import sqlite3
 
 conexao = sqlite3.connect("BD/projeto.db")
 cursor = conexao.cursor()   
-cursor.execute("SELECT * FROM venda")
+cursor.execute("SELECT * FROM produto")
 todos = cursor.fetchall()
 print(todos)
