7c7b9feb2e5d75559d4cb077feb097da8d3ddd42
Kayky-Eduardo <kayky002a@gmail.com>
2025-05-24 19:05:48 -0300

    Validações de entrada de dados + Updates + validações banco de dados + Trabalho adicional que me dei

--- projeto/cadastros/encomenda/cadastro_encomenda.py
+++ projeto/cadastros/encomenda/cadastro_encomenda.py
@@ -8,35 +8,81 @@ Builder.load_file(os.path.join(os.path.dirname(__file__), 'cadastro_encomenda.kv
 
 
 class Cadastro_Encomenda(Screen):
+    cliente_cpf = ObjectProperty(None)
     produto_id = ObjectProperty(None)
-    nome_produto = ObjectProperty(None)
-    tipo_produto = ObjectProperty(None)
-    preco_produto = ObjectProperty(None)
-    quantidade = ObjectProperty(None)
-    validade = ObjectProperty(None)
+    qntd_produto = ObjectProperty(None)
+    data_pedido = ObjectProperty(None)
+    data_entrega = ObjectProperty(None)
     enviar = ObjectProperty(None)
 
-    def adicionar_banco(self):
+    def buscar_dados(self, produto_id, cliente_cpf):
+        conexao = sqlite3.connect('BD/projeto.db')
+        cursor = conexao.cursor()
+        cursor.execute("""
+            SELECT qntd_produto FROM estoque WHERE produto_id = ?
+        """, (produto_id,))
+        produto = cursor.fetchone()
+        
+        cursor.execute("SELECT 1 FROM cliente WHERE cliente_cpf = ?", (cliente_cpf,))
+        if not cursor.fetchone():
+            self.enviar.text = "Cliente não encontrado."
+            cliente_cpf = cursor.fetchone()
+            conexao.close()
+            
+        return produto, cliente_cpf
+    
+    def adicionar_banco(self):    
+        if not self.produto_id.text.isdigit():
+            self.enviar.text = 'Preencha o campo de produto id corretamente.'
+            return
+        
+        elif not self.qntd_produto.text.isdigit():
+            self.enviar.text = 'Preencha o campo de quantidade corretamente.'
+            return
+    
+        elif not self.cliente_cpf.text:
+            self.enviar.text = 'Preencha o campo cliente CPF de forma correta.'    
+            return
+        
+        elif not self.data_pedido.text:
+            self.enviar.text = 'Preencha o campo de data do pedido corretamente.'
+            return
+          
         cliente_cpf_validacao = self.cliente_cpf.text
-        produto_id_validacao = self.produto_id.text
-        qntd_produto_validacao = self.qntd_produto.text
+        produto_id_validacao = int(self.produto_id.text)
+        qntd_produto_validacao = int(self.qntd_produto.text)
         data_pedido_validacao = self.data_pedido.text
         data_entrega_validacao = self.data_entrega.text
-        
+        quantidade, cpf = self.buscar_dados(produto_id_validacao, cliente_cpf_validacao)
+
         try:
-            conexao = sqlite3.connect('BD/projeto.db')
-            cursor = conexao.cursor()
-            cursor.execute('''
-                INSERT INTO encomenda (cliente_cpf, produto_id, qntd_produto, data_pedido, data_entrega)
-                VALUES (?, ?, ?, ?, ?)
-            ''', (cliente_cpf_validacao, produto_id_validacao, qntd_produto_validacao, data_pedido_validacao, data_entrega_validacao))
-            conexao.commit()
-            conexao.close()
-            self.enviar.text = 'Cadastro realizado com sucesso!'
-            
+            if quantidade and cpf:
+                if quantidade[0] >= int(qntd_produto_validacao):
+                    conexao = sqlite3.connect('BD/projeto.db')
+                    cursor = conexao.cursor()
+                    cursor.execute('''
+                        INSERT INTO encomenda (cliente_cpf, produto_id, qntd_produto, data_pedido, data_entrega)
+                        VALUES (?, ?, ?, ?, ?)
+                    ''', (cliente_cpf_validacao, produto_id_validacao, qntd_produto_validacao, data_pedido_validacao, data_entrega_validacao))
+                    cursor.execute('''
+                    UPDATE estoque 
+                    SET qntd_produto = qntd_produto - ? 
+                    WHERE produto_id = ?
+                    ''', (qntd_produto_validacao, produto_id_validacao))
+                    conexao.commit()
+                    conexao.close()
+                    self.enviar.text = 'Cadastro realizado com sucesso!'
+                
+                else:
+                    self.enviar.text = 'Quantidade insuficiente no estoque'
+            else:
+                self.enviar.text = 'Produto ou CPF de cliente não encontrado.'
+
         except sqlite3.Error as e:
             self.enviar.text = f'Erro: {e}'
-                    
+        
+        except ValueError:
+            self.enviar.text = f'Erro na quantidade.'
 
 # class Cadastro_EncomendaApp(App):
 #     def build(self):
